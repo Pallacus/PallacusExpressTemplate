96d8f5a9941f5b0f817f555efcf64ae03947fd50
Albert Pallach Font <albertpf@gmail.com>
2024-09-25 00:29:00 +0200

    Complete set of methods

--- src/routes/a.js
+++ src/routes/a.js
@@ -1,8 +1,23 @@
 const router = require('express').Router();
 
-// Resposta de prova
+router.post('/', (req, res) => {
+    res.end(`Create a new item`);
+});
+
 router.get('/', (req, res) => {
-    res.end(`Route A`);
+    res.end("Retrieve all data");
+});
+
+router.put('/', (req, res) => {
+    res.end('Replace an item');
+});
+
+router.patch('/', (req, res) => {
+    res.end("Partial update of an item");
+});
+
+router.delete('/', (req, res) => {
+    res.end("Delete an item");
 });
 
 module.exports = router;
