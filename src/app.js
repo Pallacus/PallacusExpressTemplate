fba49d9619947fafa992950fc73041ed24aeb5cc
Albert Pallach Font <albertpf@gmail.com>
2024-09-24 23:59:49 +0200

    Complete routes deployment

--- src/app.js
+++ src/app.js
@@ -2,9 +2,8 @@ const express = require('express');
 
 const app = express()
 
-app.use('/a', require('./routes/a.js'))
+app.use('/api', require('./routes/api'));
+app.use('/a', require('./routes/a'))
 app.use('/b', require('./routes/b'))
-app.use('/c', require('./routes/c'))
-app.use('/d', require('./routes/d'))
 
 module.exports = app;
