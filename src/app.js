3bf3b44756e26b288a0c7cd253908dad4a34f434
Albert Pallach Font <albertpf@gmail.com>
2024-09-27 15:17:06 +0200

    Added generic middlewares

--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
 const express = require('express');
+const dayjs = require('dayjs');
+const fs = require('node:fs/promises');
 
 const app = express();
 app.use(express.json());
+
+// Generic middlewares:
+app.use((req, res, next) => {
+    console.log(dayjs().format(`dddd - DD MMMM YYYY HH:mm`));
+    next();
+});
+app.use(async (req, res, next) => {
+    const actualDate = dayjs().format('DDD-MM-YYY HH:mm:ss')
+    const info = `[${actualDate}] Method: ${req.method}. Url: ${req.url}\n`;
+    await fs.appendFile('./main.log', info);
+    next();
+});
+app.use((req, res, next) => {
+    const randomNum = Math.random();
+    if (randomNum > 0.2) {
+        return res.end('Bad luck')
+    }
+    next();
+});
 
 app.use('/a', require('./routes/a'));
 app.use('/b', require('./routes/b'));
